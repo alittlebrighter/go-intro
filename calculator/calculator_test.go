37bb4305bdce0a5ebd3ba6c34338c9eda0a9204e
Adam Bright <brightam1@gmail.com>
2019-05-11 23:45:16 -0400

    add benchmark

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -12,3 +12,11 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Add.IntApply(4, 1, 2) got %d want %d", got, want)
 	}
 }
+
+// $ cd calculator
+// $ go test -bench . -benchmem
+func BenchmarkAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Add.IntApply(i, i+1, i+2)
+	}
+}
